f158d1b9190d8e7cf0441972bd3bb50aebb28557
Анна Полуэктова <a.poluektova88@gmail.com>
2021-09-06 15:06:16 +0400

    Add silders configuration for desktop version

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,27 @@
-const swiper = new Swiper('.swiper', {
+const projectsSwiper = new Swiper('.projects__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  allowTouchMove: true,
+
+  pagination: {
+    el: '.projects__swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.projects__swiper-button-next',
+    prevEl: '.projects__swiper-button-prev',
+  },
+
+  breakpoints: {
+    1340: {
+      initialSlide: 8,
+    }
+  }
+});
+
+const principlesSwiper = new Swiper('.principles__swiper', {
   direction: 'horizontal',
   loop: true,
   slidesPerView: 1,
@@ -6,7 +29,41 @@ const swiper = new Swiper('.swiper', {
   autoHeight: true,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: '.principles__swiper-pagination',
     clickable: true,
   },
+
+  navigation: {
+    nextEl: '.principles__swiper-button-next',
+    prevEl: '.principles__swiper-button-prev',
+  },
+
+  breakpoints: {
+    1340: {
+      initialSlide: 1,
+    }
+  }
+});
+
+const employeesSwiper = new Swiper('.employees__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  allowTouchMove: true,
+
+  pagination: {
+    el: '.employees__swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.employees__swiper-button-next',
+    prevEl: '.employees__swiper-button-prev',
+  },
+
+  breakpoints: {
+    1340: {
+      initialSlide: 9,
+    }
+  }
 });
